356c562f0a9ff6189b4085ce7d7b08d207b27946
Fahiye Muhammad <fahiye254@gmail.com>
2025-03-23 11:44:43 +0300

    Learning  simple algorithmic problem solving

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  word = word.toLowerCase()
+    if (word === word.split("").reverse().join("")){
+      return true
+    }else{
+      return false
+    }
 }
 
 /* 
-  Add your pseudocode here
-*/
+  convert the word to lowercase since it's case sensitive
+  splits the word into an array of single characters,reverses each letter, joins abck the letters into a word.
+  checks if it's equal to the initial word initial 
 
-/*
-  Add written explanation of your solution here
 */
+console.log(isPalindrome("racecar"))
+console.log(isPalindrome("abba"))
+console.log(isPalindrome("robot"))
+console.log(isPalindrome("a"))
+console.log(isPalindrome("ab"))
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
